d6782bba53ae7d6c197d548bd60b1c0e28bf43e2
LeleRed <lelered@gmail.com>
2019-06-13 14:09:18 +0200

    main functions

--- lib/active_support/cache/gcloud_datastore.rb
+++ lib/active_support/cache/gcloud_datastore.rb
@@ -5,7 +5,30 @@ module ActiveSupport
   module Cache
     class GcloudDatastore < Store
 
-     
+      # attr_reader :project_id
+
+      def initialize(*options)
+        super(options)
+      end
+
+
+      private
+
+      # Reads an entry from the cache implementation. Subclasses must implement
+      # this method.
+      def read_entry(key, options)
+      end
+
+      # Writes an entry to the cache implementation. Subclasses must implement
+      # this method.
+      def write_entry(key, entry, options)
+      end
+
+      # Deletes an entry from the cache implementation. Subclasses must
+      # implement this method.
+      def delete_entry(key, options)
+      end
+
     end
   end
 end
